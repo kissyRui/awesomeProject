2b7146306433c807284c04155e19f6806c1fcc3e
kissy小鬼 <892455559@qq.com>
2016-05-06 10:39:46 +0800

    changed project name

--- index.android.js
+++ index.android.js
@@ -3,49 +3,20 @@
  * https://github.com/facebook/react-native
  */
 'use strict';
+
+var Login = require('./views/practice/login');
+
 import React, {
   AppRegistry,
-  Component,
-  StyleSheet,
-  Text,
-  View
+  Component
 } from 'react-native';
 
-class AwesomeProject extends Component {
+class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          欢迎来到React Native的世界!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+      <Login/>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#e7e7e7',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
-
-AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject);
+AppRegistry.registerComponent('App', () => App);

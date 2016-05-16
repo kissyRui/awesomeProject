7c71f45f366e49360f3e4cbb99bdefd6d8e7f0ab
kissy小鬼 <892455559@qq.com>
2016-05-17 06:00:12 +0800

    view ok

--- app/views/practice/login.js
+++ app/views/practice/login.js
@@ -8,11 +8,19 @@ import React, {
     View,
     TextInput,
     Alert,
+    Modal,
     PixelRatio,
     TouchableOpacity
 } from 'react-native';
 
 class Login extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+        };
+    }
+
     render() {
         return (
             <View style={styles.main}>
@@ -21,10 +29,36 @@ class Login extends Component {
                         <View style={styles.field_label}>
                             <Text style={styles.field_label_text}>国家地区</Text>
                         </View>
-                        <View style={styles.field_input}></View>
                         <View style={styles.field_arrow}></View>
                     </View>
                 </TouchableOpacity>
+                <View style={styles.field_wrap}>
+                    <View style={styles.field_label}>
+                        <Text style={styles.field_label_text}>手机号</Text>
+                    </View>
+                    <View style={styles.field_input}>
+                        <TextInput
+                            underlineColorAndroid="rgba(0,0,0,0)"
+                            style={styles.field_input_item}
+                            placeholder="请输入手机号"
+                            defaultValue="" />
+                    </View>
+                    <TouchableOpacity style={styles.field_button}>
+                        <Text style={styles.field_button_text}>获取验证码</Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.field_wrap}>
+                    <View style={styles.field_label}>
+                        <Text style={styles.field_label_text}>验证码</Text>
+                    </View>
+                    <View style={styles.field_input}>
+                        <TextInput
+                            underlineColorAndroid="rgba(0,0,0,0)"
+                            style={styles.field_input_item}
+                            placeholder="请输入验证码"
+                            defaultValue="" />
+                    </View>
+                </View>
             </View>
         );
     }
@@ -32,24 +66,23 @@ class Login extends Component {
 
 const styles = StyleSheet.create({
         main: {
-            flex: 1,
-            flexDirection: 'row'
+            flexDirection: 'column'
         },
         field_touchable: {
-            flex: 1,
-            height: 53,
-            backgroundColor: '#EFEFEF'
+            flex: 1
         },
+        // 表单字段
         field_wrap: {
             position: 'relative',
             flex: 1,
+            height: 53,
             borderBottomWidth: 1,
             borderBottomColor: '#d9d9d9',
+            backgroundColor:'#fff',
             flexDirection: 'row'
         },
         // left
         field_label: {
-            flex: 1,
             width: 80,
             justifyContent: 'center',
             marginLeft: 15
@@ -61,6 +94,21 @@ const styles = StyleSheet.create({
         // center
         field_input: {
             flex: 1
+        },
+        field_input_item: {
+            flex: 1,
+            fontSize: 16,
+            marginTop: 10,
+            height: 42
+        },
+        field_button: {
+            height: 52,
+            paddingRight: 15,
+            justifyContent: 'center'
+        },
+        field_button_text: {
+            color: '#1e9deb',
+            fontSize: 16
         },
         // right
         field_arrow: {

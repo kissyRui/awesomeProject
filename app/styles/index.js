cbece80fbf171cb58d2af78b5394f77b0efc74d0
kissy小鬼 <892455559@qq.com>
2016-06-15 16:36:03 +0800

    suggest styles in views

---
+++ app/styles/index.js
@@ -0,0 +1,12 @@
+/**
+ * Created by kissy小鬼 on 16/6/15-下午4:28.
+ */
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+    main: {
+        flexDirection: 'column'
+    }
+})
+
+export default styles

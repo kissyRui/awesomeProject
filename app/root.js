86b3acd1e67b06ed2378a1b8b6ed14959e050618
kissy小鬼 <892455559@qq.com>
2016-06-21 09:56:22 +0800

    end up

--- app/root.js
+++ app/root.js
@@ -43,7 +43,6 @@ class Root extends Component {
     }
 
     _configureScene = (route) => {
-        //return Navigator.SceneConfigs.FadeAndroid
         return  Navigator.SceneConfigs.HorizontalSwipeJump
     };
 
